d6ea172d296856bd72d1838deb9e86d853cb9143
js25s <petrauskas.justas@gmail.com>
2021-09-06 17:45:22 +0200

    main page fix

--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -1,12 +1,18 @@
-import { Container } from "react-bootstrap";
+import { Container, Row, Col } from "react-bootstrap";
 import ProfileJumbo from "../components/ProfileJumbo";
 import Sideprofile from "../components/Sideprofile";
 
 const MainPage = () => {
   return (
     <Container>
-      <Sideprofile />
-      <ProfileJumbo />
+      <Row>
+        <Col md={9}>
+          <ProfileJumbo />
+        </Col>
+        <Col md={3}>
+          <Sideprofile />
+        </Col>
+      </Row>
     </Container>
   );
 };
