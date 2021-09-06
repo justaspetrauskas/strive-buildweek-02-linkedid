779813eca433c836228e7e643b05a33303213f03
js25s <petrauskas.justas@gmail.com>
2021-09-06 16:33:08 +0200

    missing magda's part

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-
 import "./App.css";
 
-import Footer from "./conponents/Footer";
-import Profile from "./conponents/Profile";
-    import Navigation from "./components/Navigation";
+import Footer from "./components/Footer";
+import Profile from "./components/Profile";
+import Navigation from "./components/Navigation";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
     <div>
-     <Navigation />
+      <Navigation />
       <Profile />
       <Footer />
     </div>
-
   );
 }
 
