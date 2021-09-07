af61211db96f9b6dd9843c3a008f8cc32bd24f63
kolluvinay425 <kolluvinay425@gmail.com>
2021-09-07 18:40:25 +0200

    update

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import Footer from "./conponents/Footer";
-import Profile from "./conponents/Profile";
+import "./App.css";
+
+import Footer from "./components/Footer";
+
+import Profile from "./components/Profile";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+
 import { BrowserRouter as Router, Route } from "react-router-dom";
+
 function App() {
   return (
     <div>
       <Profile />
+
       {/* <Footer /> */}
     </div>
   );
