32d22bff2730e722423b5d9c6fd7c62f96a313d0
kolluvinay425 <kolluvinay425@gmail.com>
2021-09-08 11:33:43 +0200

    fixed some issues

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import "./App.css";
 
-import Footer from "./conponents/Footer";
+import Footer from "./components/Footer";
 
-import Navigation from "./components/Navigation";
+// import Navigation from "./components/Navigation";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
@@ -11,7 +11,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
     <div>
-      <Navigation />
+      {/* <Navigation /> */}
       <MainPage />
 
       <Footer />
--- src/components/MainPage.jsx
+++ src/components/MainPage.jsx
@@ -2,17 +2,23 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProfileJumbo from "../components/ProfileJumbo";
 import Sideprofile from "../components/Sideprofile";
 import Education from "../components/Education";
+import Activity from "./Activity";
+import Experience from "./Experience";
+import DashBoard from "./Dashboard";
 
 const MainPage = () => {
   return (
     <Container>
       <Row>
-        <Col md={9} className='mt-4'>
+        <Col md={9} className="mt-4">
           <ProfileJumbo />
+          <DashBoard />
+          <Activity />
+          <Experience />
           <Education />
         </Col>
-        <Col md={3} className='mt-5'>
-          <Sideprofile /> 
+        <Col md={3} className="mt-5">
+          <Sideprofile />
         </Col>
       </Row>
     </Container>
