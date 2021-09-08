7d01c8c07cf5b76584efef76d6f56b2a4ef95177
MagdalenaES83 <84572667+MagdalenaES83@users.noreply.github.com>
2021-09-08 17:48:36 +0200

    Day3,
    updated Jumbo with Modal, HomePage - leftComponent  +  from input for posting. posting doesnt work yet .

--- src/components/ProfileJumbo.jsx
+++ src/components/ProfileJumbo.jsx
@@ -1,29 +1,38 @@
 import "../style/ProfileJumbo.css";
 import React from "react";
-import { Container, Carousel, Card } from "react-bootstrap";
-import TestCarousel from "./TestCarousel";
+import {
+  Container,
+  Carousel,
+  Card,
+  Modal,
+  Col,
+  Row,
+  Button,
+} from "react-bootstrap";
 import { Link } from "react-router-dom";
-
 import { FetchMe } from "../functions/FetchMe";
 
 class ProfileJumbo extends React.Component {
   state = {
     name: null,
-    surname:null, 
-    loading : true,
+    surname: null,
+    loading: true,
+    show: false,
   };
 
   componentDidMount = async () => {
     try {
       const user = await FetchMe();
-      this.setState({user,loading:false});
-      console.log("success", user.name); 
+      this.setState({ user, loading: false });
+      console.log("success", user.name);
     } catch (error) {
       console.log(error);
     }
   };
 
- 
+  handleClose = () => this.setState({ show: false });
+  handleShow = () => this.setState({ show: true });
+
   render() {
     return (
       <div className="profile-main border mb-3 mt-4">
@@ -42,25 +51,55 @@ class ProfileJumbo extends React.Component {
               src={this.state.loading ? "loading" : this.state.user.image}
               className="profil-rounded-img"
               style={{ height: "10rem" }}
+              onClick={this.handleShow}
             />
           </div>
 
+          <Modal id="modal" show={this.state.show} onHide={this.handleClose}>
+            {/* <Modal.Header closeButton>
+            
+          </Modal.Header> */}
+            <Modal.Body>
+              <h3>
+                Profile photo
+                <Button variant="secondary" onClick={this.handleClose}>
+                  X
+                </Button>
+              </h3>
+
+              <img
+                id="modalPic"
+                src={this.state.loading ? "loading" : this.state.user.image}
+                className="profil-rounded-img"
+                style={{ height: "15rem", width: "15rem" }}
+                onClick={this.handleShow}
+              />
+            </Modal.Body>
+            <Modal.Footer></Modal.Footer>
+          </Modal>
+
           <div className="d-flex justify-content-between ">
             <div className="personal-info  pb-3">
-              <h2>                
-                {this.state.loading ? "loading" : this.state.user.name  }  
+              <div></div>
+              <h2>
+                {this.state.loading ? "loading" : this.state.user.name}
                 <span> </span>
                 {this.state.loading ? "loading" : this.state.user.surname}
-               
               </h2>
 
-              <p>{this.state.loading ? "loading" : this.state.user.title} </p><div>  {this.state.loading ? "loading" : this.state.user.area  }</div> 
+              <p>{this.state.loading ? "loading" : this.state.user.title} </p>
+              <div>
+                {" "}
+                {this.state.loading ? "loading" : this.state.user.area}
+              </div>
 
               <p className="text-muted personal-info-text ">
-              {this.state.loading ? "loading" : this.state.user.bio}
+                {this.state.loading ? "loading" : this.state.user.bio}
               </p>
               <div className="d-flex">
-                <p className=" text-muted personal-info-text">{this.state.loading ? "loading" : this.state.user.email}</p>
+                <p className=" text-muted personal-info-text">
+                  {this.state.loading ? "loading" : this.state.user.email}
+                </p>
 
                 <p className="ml-2">
                   <a className=" personal-text-color " href="/">
@@ -94,20 +133,25 @@ class ProfileJumbo extends React.Component {
           <div className="caro">
             <Carousel>
               <Carousel.Item>
-                <Card.Body className="carousel-card">Show recruiters you're open to work - you control who sees this.  Get started  </Card.Body>
+                <Card.Body className="carousel-card">
+                  Show recruiters you're open to work - you control who sees
+                  this. Get started{" "}
+                </Card.Body>
               </Carousel.Item>
               <Carousel.Item>
                 <Card>
-                  <Card.Body className="carousel-card">Share that you’re hiring and attract qualified candidates.
-
-Get started</Card.Body>
+                  <Card.Body className="carousel-card">
+                    Share that you’re hiring and attract qualified candidates.
+                    Get started
+                  </Card.Body>
                 </Card>
               </Carousel.Item>
               <Carousel.Item>
                 <Card>
-                  <Card.Body className="carousel-card">Find potential clients by showcasing the services you provide.
-
-Get started</Card.Body>
+                  <Card.Body className="carousel-card">
+                    Find potential clients by showcasing the services you
+                    provide. Get started
+                  </Card.Body>
                 </Card>
               </Carousel.Item>
             </Carousel>
