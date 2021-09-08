74c813ca2decc316109328af324ba88097d0f563
kolluvinay425 <kolluvinay425@gmail.com>
2021-09-08 15:04:54 +0200

    update

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Footer from "./components/Footer";
 import Profile from "./components/Profile";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
+import FeedsLeftBar from "./components/FeedsLeftBar";
 
 // import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div style={{ background: "#cfcfcf" }}>
-      <Profile />
+      {/* <Profile /> */}
+      <FeedsLeftBar />
       <Footer />
       {/* <Footer /> */}
     </div>
