4a850911b28f5e160ea05f94b0a6e8788cf5e72d
kolluvinay425 <kolluvinay425@gmail.com>
2021-09-09 15:45:58 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,15 @@ import Footer from "./components/Footer";
 import Profile from "./components/Profile";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-
+// import ExperienceModal from "./components/ExperienceModal";
 // import { BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
   return (
     <div style={{ background: "#cfcfcf" }}>
-      {/* <Profile /> */}
+      {/* <ExperienceModal /> */}
       <Profile />
+
       <Footer />
       {/* <Footer /> */}
     </div>
