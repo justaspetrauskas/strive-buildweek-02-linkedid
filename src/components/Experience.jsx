c7743d42ebc7d67aadd77040b29dcfeb4426dc4e
kolluvinay425 <kolluvinay425@gmail.com>
2021-09-09 16:16:05 +0200

    small correction

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import { Row, Col, Card } from "react-bootstrap";
 import ExperienceModal from "./ExperienceModal";
-import Single from "./SingleExperience";
+import Single from "./SIngleExperience";
 const Experience = () => {
   const [userInfo, setUserInfo] = useState([]);
   const [isModalPost, setIsModalPost] = useState(false);
