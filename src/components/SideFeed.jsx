9691bcc198799d5193cf62f8bdaa087c983e8384
Zukhriddin <zuxr.akhmedov@gmail.com>
2021-09-10 03:18:44 +0500

    footer has been implemented

--- src/components/SideFeed.jsx
+++ src/components/SideFeed.jsx
@@ -60,6 +60,30 @@ const SideFeed = () => {
                             </div>
                         </Card.Body>
                     </Card>
+                    <div className="footer">
+                        <span className="footer-info">About   Accessibility   Help Center</span>
+                        <br />
+                        <span className="footer-info">Privacy & Terms<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-compact-down" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z" />
+                        </svg>    Ad Choices</span><br />
+                        <span className="footer-info">Advertising   Business Services<svg className="arrow" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-compact-down" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z" />
+                        </svg></span><br />
+                        <span className="footer-info">Get the Linkedin app    More</span>
+                        <span className="copyright">
+                            <img
+                                style={{
+                                    textAlign: "left",
+                                    width: "60px",
+                                    paddingLeft: "0px",
+                                    margin: "0px",
+                                }}
+                                src="https://loghi-famosi.com/wp-content/uploads/2020/04/Linkedin-Logo.png"
+                                alt=""
+                            /> Linkedin Corporation @2021
+
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
