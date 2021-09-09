47699ff776c51b6eef482eaf4b4ead37dfdbe917
Zukhriddin <zuxr.akhmedov@gmail.com>
2021-09-10 02:29:50 +0500

    Sideprofile new component has been implemented of people may know the user

--- src/components/Sideprofile.jsx
+++ src/components/Sideprofile.jsx
@@ -20,8 +20,7 @@ class Sideprofile extends Component {
                 });
             if (response.ok) {
                 let profiles = await response.json();
-                let profilePortion = profiles.slice(0, 6)
-                this.setState({ profiles: profilePortion })
+                this.setState({ profiles })
                 console.log(this.state.profiles)
 
             } else {
@@ -45,13 +44,15 @@ class Sideprofile extends Component {
                             <ListGroup variant="flush">
                                 <ListGroup.Item>
                                     Edit public profile & URL
-                                    <i class="bi bi-question-circle-fill">
-                                    </i>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle-fill" viewBox="0 0 16 16">
+                                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z" />
+                                    </svg>
                                 </ListGroup.Item>
                                 <ListGroup.Item>
                                     Add profile in another language
-                                    <i class="bi bi-question-circle-fill">
-                                    </i>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-question-circle-fill" viewBox="0 0 16 16">
+                                        <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.496 6.033h.825c.138 0 .248-.113.266-.25.09-.656.54-1.134 1.342-1.134.686 0 1.314.343 1.314 1.168 0 .635-.374.927-.965 1.371-.673.489-1.206 1.06-1.168 1.987l.003.217a.25.25 0 0 0 .25.246h.811a.25.25 0 0 0 .25-.25v-.105c0-.718.273-.927 1.01-1.486.609-.463 1.244-.977 1.244-2.056 0-1.511-1.276-2.241-2.673-2.241-1.267 0-2.655.59-2.75 2.286a.237.237 0 0 0 .241.247zm2.325 6.443c.61 0 1.029-.394 1.029-.927 0-.552-.42-.94-1.029-.94-.584 0-1.009.388-1.009.94 0 .533.425.927 1.01.927z" />
+                                    </svg>
                                 </ListGroup.Item>
                             </ListGroup>
                         </Card>
@@ -60,7 +61,7 @@ class Sideprofile extends Component {
                             >
                                 <Card style={{ width: '20rem' }}>
                                     <h5><bold>People also viewed</bold></h5>
-                                    {this.state.profiles.map(profile => (
+                                    {this.state.profiles.slice(23, 29).map(profile => (
                                         <> {// flexbox
                                             // you need to make this flex and then use the flex properties to do the alignment
                                             // d-flex, justify-content, align-items
@@ -89,10 +90,10 @@ class Sideprofile extends Component {
                                     )
 
                                     )}
-                                    <hr />
+                                    <hr className="hr" />
                                     <Dropdown className="dropdown">
                                         <Dropdown.Toggle variant="white">
-                                            Show more
+                                            <big>Show more</big>
                                         </Dropdown.Toggle>
                                     </Dropdown>
                                 </Card>
@@ -102,9 +103,42 @@ class Sideprofile extends Component {
 
 
                         }
+                        <Card style={{ width: '20rem' }}>
+                            <h5><bold>People you may know</bold></h5>
+                            {this.state.profiles.slice(46, 52).map(profile => (
+                                <>
+                                    <Card.Body className="profile-card-body ">
+                                        <div className="d-flex p-3">
+                                            <Card.Img variant="bottom" src={profile.image} className="profile-img" />
+                                            <a href="">
+                                                <Card.Title className="profile-info">
+                                                    <small><bold>{profile.name} {profile.surname}<br />
+
+                                                        <span className="profile-title">  {profile.title}</span>  </bold></small></Card.Title>
+
+                                            </a></div>
+
+                                        <Button
+                                            className="button1"
+                                            variant="light">Connect</Button>
+
+                                    </Card.Body>
+
+
+                                </>
+                            )
+
+                            )}
+                            <hr className="hr" />
+                            <Dropdown className="dropdown">
+                                <Dropdown.Toggle variant="white">
+                                    <big>Show more</big>
+                                </Dropdown.Toggle>
+                            </Dropdown>
+                        </Card>
                     </div>
                 </div>
-            </div>
+            </div >
         )
     }
 }
